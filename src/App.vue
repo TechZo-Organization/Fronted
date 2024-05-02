b8a574238be937df7d349be84b39356e6c1865a9
IanHD04 <u202214059@upc.edu.pe>
2024-05-02 00:44:44 -0500

    chore: Install dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,10 +7,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
