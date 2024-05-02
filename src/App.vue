b06adc4a99c2d0d78bab43b33809aa7d7aa0eb53
IanHD04 <u202214059@upc.edu.pe>
2024-05-02 01:09:29 -0500

    Feat: Footer Content
    Footer content was added, and the images used were also uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
-<script setup>
+<script>
+import FooterContent from "./public/components/footer-content.component.vue";
+
+export default {
+  components:{
+    FooterContent,
+  },
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-    </a>
+  <div id="app">
+    <footer-content></footer-content>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
---
+++ src/public/components/footer-content.component.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name:"footer-content",
+}
+</script>
+
+<template>
+  <div class="footer">
+    <div class="f-row1">
+      <div class="image-section">
+        <img src="../../../public/logo/cambiazo-logo.png" height="45" width="220"/>
+      </div>
+      <div class="link-section">
+        <router-link to="/home" class="yellow-link">Inicio</router-link><br><br>
+        <router-link to="/donations" class="yellow-link">Donaciones</router-link><br><br>
+        <router-link to="/memberships" class="yellow-link">Membresías</router-link><br><br>
+      </div>
+      <div class="media-section">
+        <h4>Redes sociales:</h4>
+        <div class="social-media">
+          <div class="circle-media">
+            <a href="https://www.instagram.com/" target="_blank"><img src="../../../public/social-media-logos/logo-instagram.png" alt="instagram-logo" width="25"></a>
+          </div>
+          <div class="circle-media">
+            <a href="https://youtube.com/" target="_blank"><img src="../../../public/social-media-logos/logo-youtube.png" alt="youtube-logo" width="25"></a>
+          </div>
+          <div class="circle-media">
+            <a href="https://www.linkedin.com/home" target="_blank"><img src="../../../public/social-media-logos/logo-linkedin.png" alt="linkedin-logo" width="25"></a>
+          </div>
+          <div class="circle-media">
+            <a href="https://es-la.facebook.com/" target="_blank"><img src="../../../public/social-media-logos/logo-facebook.png" alt="facebook-logo" width="25"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="f-row2">
+      <div class="help-links">
+        <router-link to="/help" class="gray-link">Ayuda</router-link>
+        <router-link to="/contact-us" class="gray-link">Contacto</router-link>
+        <router-link to="/terms-use" class="gray-link">Condiciones de uso</router-link>
+        <router-link to="/privacy-policies" class="gray-link">Políticas de privacidad</router-link>
+      </div>
+      <h4 style="color: #FFD146">&copy; TechZo 2024. Todos los derechos reservados.</h4>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
