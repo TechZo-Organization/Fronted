2ee8227930dfcd7132c50d89dc5fc1a4c44c92cf
HelloNerk <u20221a133@upc.edu.pe>
2024-05-02 02:47:40 -0500

    Feat: Page routing
    Routes for pages were added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script>
+import ToolbarContent from "./public/components/toolbar-content.component.vue";
 import FooterContent from "./public/components/footer-content.component.vue";
+import LoginContent from "./login/login-content.component.vue";
 
 export default {
   components:{
+    LoginContent,
     FooterContent,
+    ToolbarContent,
   },
 
 }
@@ -11,10 +15,26 @@ export default {
 
 <template>
   <div id="app">
-    <footer-content></footer-content>
+    <toolbar-content v-if="$route.path !== '/log-in' && $route.path !== '/register'
+    && $route.path !== '/admin'
+    && $route.path !== '/admin/users'
+    && $route.path !== '/admin/ongs'
+    && $route.path !== '/admin/memberships'
+    && $route.path !== '/admin/categories-product'
+    && $route.path !== '/admin/categories-ong'"
+    ></toolbar-content>
+    <router-view></router-view>
+    <footer-content v-if="$route.path !== '/log-in' && $route.path !== '/register'
+    && $route.path !== '/admin'
+    && $route.path !== '/admin/users'
+    && $route.path !== '/admin/ongs'
+    && $route.path !== '/admin/memberships'
+    && $route.path !== '/admin/categories-product'
+    && $route.path !== '/admin/categories-ong'"
+    ></footer-content>
   </div>
 </template>
 
-<style>
+<style scoped>
 
 </style>
