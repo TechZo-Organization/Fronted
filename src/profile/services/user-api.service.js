1ce41bd1266464ac5827423db9405ce7a1ff5fd5
IanHD04 <u202214059@upc.edu.pe>
2024-06-05 01:21:17 -0500

    Feat: A new method was added to the User Service
    Added a method for the Users service

--- src/profile/services/user-api.service.js
+++ src/profile/services/user-api.service.js
@@ -5,11 +5,15 @@ const http = axios.create({
     baseURL: environment.baseUrl,
 });
 
+
 export class userApiService {
     getUsers() {
         return http.get("/users");
     }
     getUserById(userId) {
         return http.get(`/users/${userId}`);
+    }
+    putUser(id,data){
+        return http.put(`/users/${id}`,data)
     }
 }
