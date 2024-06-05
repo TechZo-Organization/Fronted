c9410432051d26f452310e87c4017c0a2e460e63
diegooo01 <criollodiego542@gmail.com>
2024-06-04 20:14:21 -0500

    Feat: Created main components for the product-categories-content
    I've created the respectives components for the content component with all its structure and style

---
+++ src/product-categories/components/every-product-list.vue
@@ -0,0 +1,31 @@
+<script>
+import CardEveryProduct from "./card-every-product.vue";
+
+export default {
+  name: "every-product-list",
+  components: {
+    CardEveryProduct,
+  },
+  props: {
+    products: Array,
+    categories: Array,
+  }
+}
+</script>
+
+<template>
+  <div class="list-products">
+    <card-every-product v-for="product in products" :key="product.id" :product="product" :categories="categories"></card-every-product>
+  </div>
+</template>
+
+<style scoped>
+.list-products{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 100%;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+</style>
