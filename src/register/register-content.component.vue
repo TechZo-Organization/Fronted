182f4e19ea36e2ccdd4fffd4672ac69f3cfca7d1
diegooo01 <criollodiego542@gmail.com>
2024-06-05 15:08:28 -0500

    Fix: Added new changes
    I've added new changes and functionalities in the script to be able to register a new user

--- src/register/register-content.component.vue
+++ src/register/register-content.component.vue
@@ -1,14 +1,84 @@
 <script>
+import { userApiService } from "../login/services/user-api.service.js";
+
 export default {
   name: "register-content",
-}
+  data() {
+    return {
+      name: '',
+      email: '',
+      phone: '',
+      password: '',
+      confirmPassword: '',
+      showPassword: false,
+      showConfirmPassword: false,
+      userService: new userApiService(),
+      errorMessage: '',
+    };
+  },
+  methods: {
+    async handleRegister() {
+      if (!this.validatePhone(this.phone)) {
+        this.errorMessage = 'El número de celular debe tener 9 dígitos';
+        return;
+      }
+
+      if (!this.validatePassword(this.password)) {
+        this.errorMessage = 'La contraseña debe incluir al menos 2 números, un carácter especial y una letra mayúscula';
+        return;
+      }
+
+      if (this.password !== this.confirmPassword) {
+        this.errorMessage = 'Las contraseñas no coinciden';
+        return;
+      }
+
+      try {
+        const response = await this.userService.getUser();
+        const users = response.data;
+        const newUser = {
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          password: this.password,
+          membership: 1,
+          membership_date: '',
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6lqpQj3oAmc1gtyM78oJCbTaDrD7Fj9NRlceOPDZiHA&s",
+          id: (users.length + 1).toString(),
+          favorites: []
+        };
+
+        await this.userService.registerUser(newUser);
+        this.$router.push('/log-in');
+      } catch (error) {
+        console.error(error);
+        this.errorMessage = 'Error al registrar el usuario';
+      }
+    },
+    validatePhone(phone) {
+      const phoneRegex = /^\d{9}$/;
+      return phoneRegex.test(phone);
+    },
+    validatePassword(password) {
+      const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*\d.*\d).{8,}$/;
+      return passwordRegex.test(password);
+    },
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    },
+    toggleConfirmPasswordVisibility() {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
+};
 </script>
 
 <template>
   <div class="login-container">
     <div class="login-content">
       <div class="login-form">
-        <form action="" class="form-container">
+        <form @submit.prevent="handleRegister" class="form-container">
+          <br>
           <img src="../../public/login/cambiazo-logo.png" height="95" width="250"/>
           <div class="inputs-register">
             <h1 style="font-size: 20px; font-weight: bolder; margin-bottom: 15px;">Registrarse</h1>
@@ -23,30 +93,45 @@ export default {
               <hr class="hr-line">
             </div>
             <div class="input-content">
-              <label><b>Nombre</b></label><br>
-              <pv-input required class="input" type="text"></pv-input>
-              <br>
-              <label><b>Contraseña</b></label><br>
-              <pv-input required class="input" type="password"></pv-input>
-              <br>
-              <label><b>Repetir contraseña</b></label><br>
-              <pv-input required class="input" type="password"></pv-input>
-              <br>
-              <label><b>Correo electrónico</b></label><br>
-              <pv-input required class="input" type="text"></pv-input>
-              <br>
-              <label><b>Número de celular</b></label><br>
-              <pv-input required class="input" type="text"></pv-input>
-              <br>
-              <div class="checkbox-terms">
-                <label for="terms&conditions"><input id="terms&conditions" type="checkbox"> Aceptar <span style="color:#ffd146;">terminos y condiciones</span></label>
+              <div>
+                <label><b>Nombre</b></label><br>
+                <pv-input v-model="name" required class="input" type="text"></pv-input>
               </div>
-              <router-link to="/log-in" type="submit">
-                <pv-button  class="submit">Registrarse</pv-button>
-              </router-link>
+              <div>
+                <label><b>Correo electrónico</b></label><br>
+                <pv-input v-model="email" required class="input" type="text"></pv-input>
+              </div>
+              <div>
+                <label><b>Número de celular</b></label><br>
+                <pv-input v-model="phone" required class="input" type="text"></pv-input>
+              </div>
+              <div>
+                <label><b>Contraseña</b></label><br>
+                <div class="input-group">
+                  <pv-input v-model="password" required class="show-hide-text" :type="showPassword ? 'text' : 'password'"></pv-input>
+                  <div class="show-hide">
+                    <img :src="showPassword ? '../../public/login/show-icon.png' : '../../public/login/hide-icon.png'" @click="togglePasswordVisibility" class="show-hide-password"/>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <label><b>Repetir contraseña</b></label><br>
+                <div class="input-group">
+                  <pv-input v-model="confirmPassword" required class="show-hide-text" :type="showConfirmPassword ? 'text' : 'password'"></pv-input>
+                  <div class="show-hide">
+                    <img :src="showConfirmPassword ? '../../public/login/show-icon.png' : '../../public/login/hide-icon.png'" @click="toggleConfirmPasswordVisibility" class="show-hide-password"/>
+                  </div>
+                </div>
+              </div>
+              <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
+              <div class="checkbox-terms">
+                <label for="terms&conditions"><input id="terms&conditions" type="checkbox" required> Aceptar <span style="color:#ffd146;">terminos y condiciones</span></label>
+              </div>
+              <pv-button type="submit" class="submit">Registrarse</pv-button>
               <router-link to="/log-in">
                 <pv-button class="create-account">¿Ya tienes una cuenta? <span style="color:#ffd146; margin-left: 10px;">Inicia sesión</span></pv-button>
               </router-link>
+              <br>
             </div>
           </div>
         </form>
@@ -61,10 +146,38 @@ export default {
       </div>
     </footer>
   </div>
-
 </template>
 
+
 <style>
+
+.show-hide{
+  padding: 0.43rem;
+  background-color: #FFD146;
+  border-radius: 0px 5px 5px 0px;
+}
+
+.show-hide-password{
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+
+.show-hide-text{
+  width: 100%;
+  border: 1px solid #ccc;
+  border-radius: 5px 0px 0px 5px;
+  padding: 0.25rem;
+}
+
+.show-hide-text:focus{
+  outline: none;
+}
+
+.show-hide-text:focus{
+  border: 1px solid #FFD146;
+}
+
 .login-container{
   width:100%;
   height: 100%;
@@ -86,7 +199,7 @@ export default {
 
 .main-image-register{
   width: 45%;
-  height: 115vh;
+  height: 132vh;
 }
 
 footer{
@@ -189,6 +302,11 @@ footer{
   width: 100%;
   justify-content: center;
   padding-top: 1rem;
+}
+
+.input-group{
+  display: flex;
+  align-items: center;
 }
 
 @media (max-width: 960px){
