b3d5277a48bae510b8c5c7a310978c496cba029f
123-Mathias <u202216282@upc.edu.pe>
2024-06-26 15:27:59 -0500

    Fix: Membership connection
    In this part, we fixed service membership.

--- src/memberships/services/membership-api.service.js
+++ src/memberships/services/membership-api.service.js
@@ -7,12 +7,12 @@ const http = axios.create({
 
 export class membershipsApiService {
     getMemberships() {
-        return http.get('/memberships');
+        return http.get('/api/v1/membership');
     }
     deleteMemberships(id){
-        return http.delete(`/memberships/${id}`)
+        return http.delete(`/api/v1/membership/${id}`)
     }
     getMembershipsById(id){
-        return http.get(`/memberships/${id}`)
+        return http.get(`/api/v1/membership/${id}`)
     }
 }
